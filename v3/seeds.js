152d9cab65ba6b99ff1849eb2a7ab574bd1d5d45
Manzi Guevara <manziguevara@gmail.com>
2019-10-18 01:18:40 +0200

    Add sidebar to show page, display comments nicely

--- v3/seeds.js
+++ v3/seeds.js
@@ -7,18 +7,21 @@ var data = [
     name: 'Clouds Rest',
     image:
       'https://cdn.vox-cdn.com/thumbor/-JoPdcgAuLTUsWiDZ62CX4wb33k=/0x0:5225x3479/1200x800/filters:focal(2195x1322:3031x2158)/cdn.vox-cdn.com/uploads/chorus_image/image/54137643/camping_tents.0.jpg',
-    description: 'Best place to settle and to relax'
+    description:
+      'vkvbsdjvkjfvsjfdvhsfdvjbndlvblvdb fgbnkfvndfbdfnvdkbndfkvbdnfbnflsbdfbndfvbfdklsvbnfbnf,bndfkvbdnjvbndn d sldfkb skb sdlk d v vf bdfbvnbksvlfkdbsd vvsdbsd vd d bsdvdfvmd vbnsdvnl dvlssbvskbfvlsdbvnspdvbhvbdvsldvbsvjvbsldvbdblds'
   },
   {
     name: 'Moutain of Trees',
     image: 'http://ladybugforgirls.org/wp-content/uploads/2018/05/Camping.jpg',
-    description: 'Lots of trees around here'
+    description:
+      'vkvbsdjvkjfvsjfdvhsfdvjbndlvblvdb fgbnkfvndfbdfnvdkbndfkvbdnfbnflsbdfbndfvbfdklsvbnfbnf,bndfkvbdnjvbndn d sldfkb skb sdlk d v vf bdfbvnbksvlfkdbsd vvsdbsd vd d bsdvdfvmd vbnsdvnl dvlssbvskbfvlsdbvnspdvbhvbdvsldvbsvjvbsldvbdblds'
   },
   {
     name: 'Desert Mesa',
     image:
       'https://www.visitstaugustine.com/sites/default/files/styles/large/public/taxonomy/faver_dykes_st_augustine_florida.jpg',
-    description: 'Best place to camp in California'
+    description:
+      'vkvbsdjvkjfvsjfdvhsfdvjbndlvblvdb fgbnkfvndfbdfnvdkbndfkvbdnfbnflsbdfbndfvbfdklsvbnfbnf,bndfkvbdnjvbndn d sldfkb skb sdlk d v vf bdfbvnbksvlfkdbsd vvsdbsd vd d bsdvdfvmd vbnsdvnl dvlssbvskbfvlsdbvnspdvbhvbdvsldvbsvjvbsldvbdblds'
   }
 ];
 function seedDb() {
