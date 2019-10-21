42d84ec3aa554a9936f8c8e6b14bf1456472047c
Manzi Guevara <manziguevara@gmail.com>
2019-10-21 23:40:53 +0200

    Flash Messages Helpful Errors

--- v3/middleware/index.js
+++ v3/middleware/index.js
@@ -6,17 +6,20 @@ middlewareObj.checkOwnership = function(req, res, next) {
   if (req.isAuthenticated()) {
     Campground.findById(req.params.id, function(err, foundCampground) {
       if (err) {
+        req.flash('error', 'Campground not found');
         res.redirect('back');
       } else {
         // does user own the campground?
         if (foundCampground.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash('error', 'You dont have permission to do that');
           res.redirect('back');
         }
       }
     });
   } else {
+    req.flash('error', 'You need to be logged in to do that');
     res.redirect('back');
   }
 };
@@ -31,11 +34,13 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
         if (foundComment.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash('error', 'You dont have permission to do that');
           res.redirect('back');
         }
       }
     });
   } else {
+    req.flash('error', 'You need to be logged to do that');
     res.redirect('back');
   }
 };
@@ -43,7 +48,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  req.flash('error', 'Please login First!');
+  req.flash('error', 'You have to be logged in to do that!');
   res.redirect('/login');
 };
 module.exports = middlewareObj;
